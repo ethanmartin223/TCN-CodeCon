0e7ea8f5605edd402c53e41e80f8722fb012ca86
ethanmartin223 <80828278+ethanmartin223@users.noreply.github.com>
2024-09-27 22:21:07 -0400

    Can no longer castle through check. Fixed several other bugs with castling

--- src/ChessBoardComponents/ChessPeices/King.java
+++ src/ChessBoardComponents/ChessPeices/King.java
@@ -16,46 +16,65 @@ public class King extends ChessPiece{
         else if (this.color == ChessPiece.WHITE) renderImage = ChessPieceImage.WHITE_KING;
     }
 
+    public boolean isInCheck() {
+        boolean inCheck = false;
+        for (ChessPiece cp : board.getAllPieces()) {
+            if (cp != this && cp.getColor() != this.color) {
+                for (int[] move : cp.getAvailableMoves(true)) {
+                    if (move[0] ==x && move[1] == y) {
+                        inCheck = true;
+                    }
+                }
+            }
+        }
+        return inCheck;
+    }
 
     @Override
     public ArrayList<int[]> getAvailableMoves(boolean b) {
         ArrayList<int[]> output = getMovesForDefinedMovements(directions, b);
-        if (!hasMovedBefore) {
-            if (color == WHITE) {
-                ChessPiece rook1 = board.getPieceAt(0, 7);
-                ChessPiece rook2 = board.getPieceAt(7, 7);
-                if (rook1.getClass() == Rook.class && !rook1.hasMovedBefore
-                        && board.getPieceAt(1, 7) == null
-                        && board.getPieceAt(2, 7) == null
-                        && board.getPieceAt(3, 7) == null) {
-                    output.add(new int[]{2, 7});
-                }
-                if (rook2.getClass() == Rook.class && !rook2.hasMovedBefore
-                        && board.getPieceAt(5, 7) == null
-                        && board.getPieceAt(6, 7) == null) {
-                    output.add(new int[]{6, 7});
-                }
 
-            } else if (color == BLACK) {
-                ChessPiece rook1 = board.getPieceAt(0, 0);
-                ChessPiece rook2 = board.getPieceAt(7, 0);
-                if (rook1.getClass() == Rook.class && !rook1.hasMovedBefore
-                        && board.getPieceAt(1, 0) == null
-                        && board.getPieceAt(2, 0) == null
-                        && board.getPieceAt(3, 0) == null) {
-                    output.add(new int[]{2, 0});
-                }
-                if (rook2.getClass() == Rook.class && !rook2.hasMovedBefore
-                        && board.getPieceAt(5, 0) == null
-                        && board.getPieceAt(6, 0) == null) {
-                    output.add(new int[]{6, 0});
-                }
-            }
-
+        if (!b && !hasMovedBefore && !isInCheck()) {
+            if (color == WHITE) addMovesForCastle(output, 7);
+            else if (color == BLACK) addMovesForCastle(output,0);
         }
         return output;
     }
 
+    private void addMovesForCastle(ArrayList<int[]> output, int yCoord) {
+        ChessPiece rook1 = board.getPieceAt(0, yCoord);
+        ChessPiece rook2 = board.getPieceAt(7, yCoord);
+        if (rook1.getClass() == Rook.class && !rook1.hasMovedBefore
+                && board.getPieceAt(1, yCoord) == null
+                && board.getPieceAt(2, yCoord) == null
+                && board.getPieceAt(3, yCoord) == null) {
+            if (isValidCastle(new int[][]{{2, yCoord}, {3, yCoord}}))
+                output.add(new int[]{2, yCoord});
+        }
+        if (rook2.getClass() == Rook.class && !rook2.hasMovedBefore
+                && board.getPieceAt(5, yCoord) == null
+                && board.getPieceAt(6, yCoord) == null) {
+            if (isValidCastle(new int[][]{{5, yCoord}, {6,yCoord}}))
+                output.add(new int[]{6, yCoord});
+        }
+    }
+
+    private boolean isValidCastle(int[][] moves) {
+        boolean canMoveWithoutBeingInCheck = true;
+        for (int[] kingMove: moves) {
+            for (ChessPiece cp : board.getAllPieces()) {
+                if (cp != this && cp.getColor() != this.color) {
+                    for (int[] move : cp.getAvailableMoves(true)) {
+                        if (move[0] == kingMove[0] && move[1] == kingMove[1]) {
+                            canMoveWithoutBeingInCheck = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return canMoveWithoutBeingInCheck;
+    }
 
     public ArrayList<int[]> getAvailableMoves() {
         return getAvailableMoves(false);
